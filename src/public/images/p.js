13ab354524d72ad4c80b2fdd63222250a88dcb6e
Glen Kelley <gisaack@byui.edu>
2025-06-05 13:17:09 -0600

    pride and prejudice init

---
+++ src/public/images/p.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", (event) => {
+    const myname = this.name.value;
+    const myemail = this.email.value;
+    let errorMessage = "";
+})
---
+++ src/week7/pride.js
@@ -0,0 +1,24 @@
+function quoteList() {   
+    const list = document.querySelector('.quotes_list');
+    list.innerHTML = '';
+    const quotesData = fetch('./quotes.json').then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to fetch JSON: ' + response.statusText);
+        }
+        return response.json();
+    });
+    const books = quoteData[1];
+    const allQuotes = [];
+    books.forEach(book => {
+        const quotes = book.quotes;
+        quotes.forEach(quote => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `${quote}`;
+            list.appendChild(listItem);
+            allQuotes.push(quote);
+        });
+    });
+    localStorage.setItem('quotes', JSON.stringify(allQuotes));
+}
+
+document.querySelector('#load_quotes').addEventListener('click', quoteList());
