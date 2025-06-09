149b0649d1af96350331262381f8f6f6584f37c6
Glen Kelley <gisaack@byui.edu>
2025-06-09 10:23:46 -0600

    vite update attempt

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   root: "src/",
 
+  publicDir: resolve(__dirname, "src/public"),
+
   build: {
     outDir: "../dist",
     rollupOptions: {
